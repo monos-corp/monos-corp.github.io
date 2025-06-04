6f86ca34aeb51182797ab9903f2bf626272c8a67
monos-corp <154058234+monos-corp@users.noreply.github.com>
2025-06-04 15:54:46 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4569,7 +4569,7 @@ persistentClock.addEventListener('click', () => {
 });
 
 document.getElementById("versionButton").addEventListener("click", function() {
-    window.open("https://kirbindustries.gitbook.io/gurasuraisu", "_blank");
+    window.open("https://sites.google.com/view/monos-home/announcements", "_blank");
 });
 
 document.addEventListener('keydown', (event) => {
